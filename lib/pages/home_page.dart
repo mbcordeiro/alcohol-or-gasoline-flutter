49653be73d1b72877c5ab9af2d46f8332ab7236e
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-25 23:02:50 -0300

    Submit form

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:alcohol_or_gasoline/widgets/input.widget.dart';
 import 'package:alcohol_or_gasoline/widgets/loading-button.widget.dart';
 import 'package:alcohol_or_gasoline/widgets/logo.widget.dart';
+import 'package:alcohol_or_gasoline/widgets/sucess.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -14,52 +15,6 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  'Compensa utilizar álcool',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: 'Big Shoulders Display',
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                  busy: false,
-                  func: () {},
-                  invert: true,
-                  text: 'CALCULAR NOVAMENTE',
-                ),
-              ],
-            ),
-          ),
-          Input(
-            label: 'Gasolina',
-            ctrl: _gasCtrl,
-          ),
-          Input(
-            label: 'Álcool',
-            ctrl: _alcCtrl,
-          ),
-          LoadingButton(
-            busy: true,
-            func: () {},
-            invert: false,
-            text: 'CALCULAR',
-          ),
         ],
       ),
     );
